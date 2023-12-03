6bf6699287f96debcbf1a8f06155ef341a54e174
erykciepiela <eryk.ciepiela.dev@gmail.com>
2023-12-03 11:38:25 +0100

    day 3 using set

--- 2023/src/Gondola.hs
+++ 2023/src/Gondola.hs
@@ -10,10 +10,12 @@ import Utils
 import Data.Sequence (mapWithIndex, fromList)
 import Control.Monad (join)
 import Data.List (groupBy, sortOn)
+import Data.Set (Set)
+import qualified Data.Set as Set
 
 -- >>> answer 2023 3 1 sumOfAllPartNumbers
 -- 531932
-sumOfAllPartNumbers input = sum $ labels input <&> number
+sumOfAllPartNumbers input = sum $ labels input & Set.map number
 
 partMap :: String -> Map.Map (Int, Int) Part
 partMap input = parts input <&> (\p@(Part ch y x) -> ((y, x), p)) & Map.fromList
@@ -22,12 +24,12 @@ parts :: String -> [Part]
 parts input = join $ toList $ mapWithIndex (\y -> cast . parse (partsParser (y + 1) 1) "") (fromList $ lines input)
 
 data Part = Part { symbol :: Char, y :: Int, x :: Int } deriving (Show, Eq, Ord)
-data Label = Label { number :: Int, part :: Part } deriving (Show, Eq)
+data Label = Label { number :: Int, ly :: Int, lx :: Int, part :: Part } deriving (Show, Eq, Ord)
 
 partsParser :: Int -> Int -> Parsec String a [Part]
 partsParser y x' = do
     paddingLength <- length <$> many (digit <|> char '.')
-    let x = x' + paddingLength 
+    let x = x' + paddingLength
     mPartSymbol <- (anyChar <&> Just) <|> (eof $> Nothing)
     case mPartSymbol of
         Nothing -> pure []
@@ -45,17 +47,18 @@ labelsParser partsMap y x' = do
         Just symbol -> do
             let len = length symbol
             labels <- labelsParser partsMap y (x + len)
-            let parts = catMaybes $ ([(y, x - 1), (y - 1, x - 1), (y + 1, x - 1), (y, x + len), (y - 1, x + len), (y + 1, x + len)] <> ([0..(len-1)] >>= (\pos -> [(y - 1, x + pos), (y + 1, x + pos)]))) <&> (\coords -> Map.lookup coords partsMap)
+            let parts = mapMaybe (`Map.lookup` partsMap) ([(y, x - 1), (y - 1, x - 1), (y + 1, x - 1), (y, x + len), (y - 1, x + len), (y + 1, x + len)] <> ([0..(len - 1)] >>= (\pos -> [(y - 1, x + pos), (y + 1, x + pos)])))
             case parts of
                 [] -> pure labels
-                part:[] -> pure $ Label (read symbol) part: labels
+                part:[] -> pure $ Label (read symbol) y x part: labels
                 _ -> error $ "too many parts: " <> show parts <> " for label " <> symbol <> " at " <> show y <> " " <> show x
 
-labels :: String -> [Label]
-labels input = join $ toList $ mapWithIndex (\y -> cast . parse (labelsParser (partMap input) (y + 1) 1) "") (fromList $ lines input)
+labels :: String -> Set Label
+labels input = Set.fromList $ join $ toList $ mapWithIndex (\y -> cast . parse (labelsParser (partMap input) (y + 1) 1) "") (fromList $ lines input)
 
 -- >>> answer 2023 3 2 sumOfGearRatios
 -- 73646890
-sumOfGearRatios input = sum (labels input & filter (\l -> (symbol . part) l  == '*') & sortOn part & groupBy ((==) `on` part) & filter ((==) 2 . length) <&> gearRatio)
+sumOfGearRatios input = labels input & Set.filter (\l -> (symbol . part) l  == '*') & (\set -> Set.cartesianProduct set set) & Set.filter (uncurry (<)) & Set.filter onTheSamePart & Set.toList <&> gearRatio & sum
     where
-        gearRatio = product . fmap number
+        onTheSamePart (label1, label2) = on (==) part label1 label2
+        gearRatio (label1, label2) = on (*) number label1 label2
