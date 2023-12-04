3c26bd465899e2a44230428b5137393c57bb7302
erykciepiela <eryk.ciepiela.dev@gmail.com>
2023-12-04 21:22:54 +0100

    dey 5 prep

---
+++ 2023/src/New.hs
@@ -0,0 +1,19 @@
+module New where
+
+import Text.Parsec
+import Data.Functor
+import qualified Data.Map as Map
+import Data.Foldable
+import Data.Maybe
+import Data.Function
+import Utils
+import Data.Sequence (mapWithIndex, fromList, sort)
+import Control.Monad (join, replicateM)
+import Data.Set (Set)
+import qualified Data.Set as Set
+
+-- >>> answer 2023 5 1 new1
+new1 input = 0
+
+-- >>> answer 2023 5 2 new2
+new2 input = 0
