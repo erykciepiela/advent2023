b8ea26da38da9f3651a43294f448524cdc1f33ea
erykciepiela <eryk.ciepiela.dev@gmail.com>
2023-12-04 06:37:15 +0100

    day 4 - 1

--- 2023/src/New.hs
+++ 2023/src/New.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE ScopedTypeVariables #-}
 module New where
 
 import Text.Parsec
@@ -8,12 +9,42 @@ import Data.Maybe
 import Data.Function
 import Utils
 import Data.Sequence (mapWithIndex, fromList)
-import Control.Monad (join)
+import Control.Monad (join, replicateM)
 import Data.Set (Set)
 import qualified Data.Set as Set
 
 -- >>> answer 2023 4 1 foo
-foo input = 0
+-- 21105
+pileOfCardsWorth input = sum $ cardWorth <$> lines input
+
+cardWorth :: String -> Int
+cardWorth card = cast $ parse cardParser "" card
+
+cardParser :: Parsec   String   u   Int
+cardParser = do
+    string "Card"
+    many1 (char ' ')
+    many1 digit
+    string ": "
+    (winningNumbers :: [Int]) <- replicateM 10 $ do
+        many (char ' ')
+        read <$> many1 digit
+    string " | "
+    (numbersIHave :: [Int]) <- replicateM 25 $ do
+        many (char ' ')
+        read <$> many1 digit
+    let matchingNumbers = Set.size $ Set.intersection (Set.fromList winningNumbers) (Set.fromList numbersIHave)
+    pure $ case matchingNumbers of
+        0 -> 0
+        n -> 2 ^ (n - 1)
+
+
+
+
+
+
+
+
 
 
 -- >>> answer 2023 4 2 foo
