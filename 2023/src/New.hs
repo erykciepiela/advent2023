76fc4aa934b109074bbef702637760b61e5f1e42
erykciepiela <eryk.ciepiela.dev@gmail.com>
2023-12-06 22:14:29 +0100

    new template

--- 2023/src/New.hs
+++ 2023/src/New.hs
@@ -2,17 +2,18 @@ module New where
 
 import Text.Parsec
 import Data.Functor
+import Data.Map (Map)
 import qualified Data.Map as Map
 import Data.Foldable
 import Data.Maybe
 import Data.Function
 import Utils
-import Data.Sequence (mapWithIndex, fromList, sort)
-import Control.Monad (join, replicateM)
+import Data.Sequence
+import Control.Monad
 import Data.Set (Set)
 import qualified Data.Set as Set
 import Control.Arrow
-import Data.List (groupBy, sortOn, singleton)
+import Data.List
 
 -- >>> answer 2023 7 1 new
 -- 1
